cc555d5692ec87eb8a41ba2f236f1c5cea85dfd5
Ishaan Patel <ipatel9@stevens.edu>
2019-05-20 14:04:10 -0400

    Created repository

---
+++ src/sum.java
@@ -0,0 +1,19 @@
+/**
+ * Ishaan Patel
+ * 5/20/19
+ * Daily Coding Problem
+ * Given a list of numbers and a value k, return if any 2
+ * numbers from the list add up to k
+ **/
+
+public class sum {
+    public boolean isSum(int[] lst, int sum) {
+
+        return false;
+    }
+
+    public static void main(String [] args) {
+        int[] test = new int[]{10, 15, 3, 7};
+        int[] nope = new int[]{1, 2, 4, 5};
+    }
+}
