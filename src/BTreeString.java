ea3db5b8f9c604f651b656729c89f8804d69cc4f
Ishaan Patel <ipatel9@stevens.edu>
2019-05-22 18:09:08 -0400

    Finished Part 1

--- src/BTreeString.java
+++ src/BTreeString.java
@@ -6,30 +6,57 @@
  * a method which serializes the tree into a string, and
  * another method which does the opposite.
  */
-public class BTreeString<E> {
-    private static class Node<E>{
-        public E val;
-        public Node<E> left;
-        public Node<E> right;
+public class BTreeString {
+    private static class Node<String>{
+        public String val;
+        public Node<String> left;
+        public Node<String> right;
 
-        public Node(E val, Node<E> left, Node<E> right) {
+        public Node(String val) {
+            this.val = val;
+            left = null;
+            right = null;
+        }
+
+        public Node(String val, Node<String> left) {
+            this.val = val;
+            this.left = left;
+            right = null;
+        }
+
+        public Node(String val, Node<String> left, Node<String> right) {
             this.val = val;
             this.left = left;
             this.right = right;
         }
     }
 
-    private Node<E> root;
 
-    public static String serialize(Node<E> current) {
+    public static String serialize(Node<String> current) {
+        StringBuilder s = new StringBuilder();
+        if (current == null)
+            return "--";
 
+        s.append("Node(" + current.val + ", ");
+        s.append(serialize(current.left));
+        s.append(", ");
+        s.append(serialize(current.right));
+        s.append(")");
+
+        return s.toString();
     }
 
-    public static BTreeString deserialie(String s) {
-
+    public static Node<String> deserialize(String s) {
+        Node<String> n = new Node<String>(null);
+        return null;
     }
 
     public static void main(String[] args) {
-        
+        Node<String> test = new Node<String>("root",
+                new Node<String>("left",
+                        new Node<String>("left.left")),
+                new Node<String>("right"));
+
+        System.out.println(serialize(test));
     }
 }
