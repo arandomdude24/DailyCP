62cfda8cfbf40848bee86b14884d29d380f22f30
Ishaan Patel <ipatel9@stevens.edu>
2019-05-25 17:10:24 -0400

    Added some code

--- src/XORLL/XORLL.java
+++ src/XORLL/XORLL.java
@@ -1,4 +1,59 @@
 package XORLL;
 
-public class XORLL {
+/**
+ * Ishaan Patel
+ * 5/25/19
+ * Daily Coding Problem #6 (Google) (Hard)
+ * Make an XOR Linked List with an add and get method
+ */
+
+public class XORLL<E> {
+    class Node<F> {
+        private boolean both;
+        private E data;
+
+        Node() {
+            data = null;
+            both = false;
+        }
+
+        Node(E data) {
+            this.data = data;
+            both = false;
+        }
+
+        Node(E data, boolean both) {
+            this.data = data;
+            this.both = both;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public boolean getBoth() {
+            return both;
+        }
+    }
+
+    private Node<E> head;
+    public int size;
+
+    XORLL() {
+        size = 0;
+        head = null;
+    }
+
+    public void add(Node<E> current) {
+        if (size == 0) {
+            head = current;
+            head.both = false;
+        }
+        else {
+            Node<E> lr = head;
+            while (lr.both) {
+
+            }
+        }
+    }
 }
