62fe6cf8d1c80594631c6a4bf4ab130c351ba99f
Ishaan Patel <ipatel9@stevens.edu>
2019-05-26 16:16:12 -0400

    Created files

---
+++ src/integerDecoder/intDecoder.java
@@ -0,0 +1,4 @@
+package integerDecoder;
+
+public class intDecoder {
+}
--- src/integerDecoder/intDecoderTest.java
+++ src/integerDecoder/intDecoderTest.java
@@ -1,7 +1,7 @@
-package XORLL;
+package integerDecoder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class XORLLTest {
+class intDecoderTest {
 
 }
