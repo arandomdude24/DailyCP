3bf59fc7ca1749814b08eba666cd9ba7edd749ed
Ishaan Patel <ipatel9@stevens.edu>
2019-05-28 17:38:12 -0400

    Finished Problem 9

--- src/nonAdjacentSum/nonAdjacentSum.java
+++ src/nonAdjacentSum/nonAdjacentSum.java
@@ -1,4 +1,35 @@
 package nonAdjacentSum;
+import java.lang.Math;
+import java.util.Arrays;
 
+/**
+ * Ishaan Patel
+ * 5/28/19
+ * Daily Coding Problem #9 (Airbnb) (Hard)
+ * Return the largest sum of non-adjacent numbers
+ * Numbers can also be 0 and negative
+ */
 public class nonAdjacentSum {
+    private int maxSumHelper(int[] lst) {
+        if (lst == null || lst.length == 0)
+            return 0;
+        else if (lst.length == 1)
+            return lst[0];
+        else if (lst.length == 2)
+            return Math.max(lst[0], lst[1]);
+        return Math.max(lst[0] + maxSumHelper(Arrays.copyOfRange(lst, 2, lst.length)),
+                    lst[0] + maxSumHelper(Arrays.copyOfRange(lst, 3, lst.length)));
+    }
+
+    public int maxSum(int[] lst) {
+        if (lst == null || lst.length == 0)
+            return 0;
+        else if (lst.length == 1)
+            return lst[0];
+        else if (lst.length == 2)
+            return Math.max(lst[0], lst[1]);
+
+        return Math.max(maxSumHelper(lst),
+                maxSumHelper(Arrays.copyOfRange(lst, 1, lst.length)));
+    }
 }
--- src/nonAdjacentSum/nonAdjacentSumTest.java
+++ src/nonAdjacentSum/nonAdjacentSumTest.java
@@ -1,7 +1,24 @@
 package nonAdjacentSum;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class nonAdjacentSumTest {
 
+    @Test
+    void maxSum() {
+        int[] empty = new int[]{};
+        int[] one = new int[]{2};
+        int[] two = new int[]{4, 8};
+        int[] test = new int[]{2, 4, 6, 2, 5};
+        int[] another = new int[]{5, 1, 1, 5};
+        nonAdjacentSum n = new nonAdjacentSum();
+
+        assertEquals(n.maxSum(empty), 0);
+        assertEquals(n.maxSum(one), 2);
+        assertEquals(n.maxSum(two), 8);
+        assertEquals(n.maxSum(test), 13);
+        assertEquals(n.maxSum(another), 10);
+    }
 }
