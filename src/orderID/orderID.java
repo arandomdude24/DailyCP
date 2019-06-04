3966e2c05bcaa8a97897b095e53c2d1f9e2fc359
Ishaan Patel <ipatel9@stevens.edu>
2019-06-04 15:11:55 -0400

    Finished Problem 16

--- src/orderID/orderID.java
+++ src/orderID/orderID.java
@@ -1,5 +1,7 @@
 package orderID;
 
+import java.util.ArrayList;
+
 /**
  * Ishaan Patel
  * 6/4/19
@@ -9,6 +11,71 @@ package orderID;
  * 2. Gets the ith last element from the log
  * Be efficient with time and space
  */
-public class orderID {
+public class orderID<E> {
+    class Node<F> {
+        private F data;
+        private Node<F> next;
+        private Node<F> prev;
+
+        Node(F data) {
+            this.data = data;
+            next = null;
+            prev = null;
+        }
+
+        Node(F data, Node<F> next, Node<F> prev) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        public String toString(){
+            return data.toString();
+        }
+    }
+
+    public Node<E> head;
+    public Node<E> tail;
+    public int size;
+
+    orderID() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public void record(E order_id) {
+        if (size == 0) {
+            head = new Node<E>(order_id);
+            head.next = null;
+            head.prev = null;
+            tail = head;
+        }
+        else if (size == 1) {
+            Node<E> insert = new Node<E>(order_id);
+            head.next = insert;
+            insert.prev = head;
+            tail = insert;
+        }
+        else {
+            Node<E> insert = new Node<E>(order_id);
+            Node<E> change = tail;
+            change.next = insert;
+            insert.prev = change;
+            tail = insert;
+        }
+        size++;
+    }
+
+    public E getLast(int i) {
+        return getLast(i, tail);
+    }
+    private E getLast(int i, Node<E> lr) {
+        if (i > 1) {
+            lr = lr.prev;
+            return getLast(i-1, lr);
+        }
+        return lr.data;
+    }
 
 }
--- src/orderID/orderIDTest.java
+++ src/orderID/orderIDTest.java
@@ -1,7 +1,23 @@
 package orderID;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class orderIDTest {
+    orderID<Integer> n = new orderID<Integer>();
+
+    @Test
+    void atest() {
+        n.record(5);
+        assertEquals(n.getLast(1), 5);
+
+        n.record(7);
+        n.record(3);
+        n.record(4);
+
+        assertEquals(n.getLast(2),3);
+        assertEquals(n.getLast(4), 5);
+    }
 
 }
