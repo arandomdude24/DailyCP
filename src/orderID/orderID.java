490e6c92c47d1bb9e84b0b3dc5ec1f03c5861646
Ishaan Patel <ipatel9@stevens.edu>
2019-06-04 14:23:45 -0400

    Created files

---
+++ src/orderID/orderID.java
@@ -0,0 +1,14 @@
+package orderID;
+
+/**
+ * Ishaan Patel
+ * 6/4/19
+ * Daily Coding Problem #16 (Twitter)(Easy)
+ * Implement a data structure that does two things
+ * 1. Adds ids to the log
+ * 2. Gets the ith last element from the log
+ * Be efficient with time and space
+ */
+public class orderID {
+
+}
---
+++ src/orderID/orderIDTest.java
@@ -0,0 +1,7 @@
+package orderID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class orderIDTest {
+
+}
