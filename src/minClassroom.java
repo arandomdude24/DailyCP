ce4accc5063099906395584ff9f7e24c2faba277
Ishaan Patel <ipatel9@stevens.edu>
2019-06-10 14:57:40 -0400

    Finished problem 21

--- src/minClassroom.java
+++ src/minClassroom.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * This problem was asked by Snapchat.
  * Given an array of time intervals (start, end) for classroom lectures
@@ -5,4 +7,39 @@
  * For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
  */
 public class minClassroom {
+    //A brute force solution that checks every single minute
+    private static int classrom(int[][] lst) {
+        int min = lst[0][0];
+        int max = -1;
+        int result = 0;
+
+        //Gets the earliest time classes start
+        //And the latest time classes end
+        for (int[] x: lst) {
+            for (int y: x) {
+                if (y < min)
+                    min = y;
+                if (y > max)
+                    max = y;
+            }
+        }
+
+        //Checks how many classrooms are needed each minute
+        //The highest number is our answer
+        for (int i=min; i<=max; i++) {
+            int temp = 0;
+            for (int[] x: lst) {
+                if (i >= x[0] && i <= x[1])
+                    temp++;
+            }
+            if (temp > result)
+                result = temp;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] test = new int[][]{{30, 75}, {0, 50}, {60, 150}};
+        System.out.println(classrom(test)); //Should return 2
+    }
 }
